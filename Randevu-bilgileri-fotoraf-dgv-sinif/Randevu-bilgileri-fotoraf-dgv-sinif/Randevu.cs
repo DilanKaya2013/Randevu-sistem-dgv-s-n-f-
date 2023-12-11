cbecfefe16dcd702b21bc8499e09b40b401259aa
DilanKaya <145452308+DilanKaya2013@users.noreply.github.com>
2023-12-11 12:13:04 +0300

    Add files via upload

---
+++ Randevu-bilgileri-fotoraf-dgv-sinif/Randevu-bilgileri-fotoraf-dgv-sinif/Randevu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Randevu_bilgileri_fotoraf_dgv_sinif
+{
+    internal class Randevu
+    {
+        int id;
+    }
+}
